61f1eebea859cc1b464223808aa352e408bd090b
stsk <stsk3@hotmail.com>
2021-10-10 02:26:06 +0800

    CHANGE layout CHANGE typhoon link

--- serviceworker.js
+++ serviceworker.js
@@ -25,7 +25,7 @@ const filesToCache = [
     "script/bus-route.js"
 ];
 
-const swVersion = 1.94;
+const swVersion = 1.95;
 const cacheName = 'stsk-portal-v' + swVersion;
 const dataCacheName = 'stsk-portal-data-v' + swVersion;
 
