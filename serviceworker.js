91a592c65932d5ba1c81f9febf386cbc32f4e954
stsk <stsk3@hotmail.com>
2021-10-12 00:00:40 +0800

    CREATE first version of bus eta

--- serviceworker.js
+++ serviceworker.js
@@ -25,7 +25,7 @@ const filesToCache = [
     "script/bus-route.js"
 ];
 
-const swVersion = 1.97;
+const swVersion = 1.100;
 const cacheName = 'stsk-portal-v' + swVersion;
 const dataCacheName = 'stsk-portal-data-v' + swVersion;
 
