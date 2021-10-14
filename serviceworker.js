3a0d4dfec357dd6cabdf48f787ac157a90952d7f
stsk <stsk3@hotmail.com>
2021-10-14 16:45:42 +0800

    FIX bus stop eta ADD refreshing div to mtr and bus

--- serviceworker.js
+++ serviceworker.js
@@ -32,7 +32,7 @@ const filesToCache = [
     "script/bus-route.js"
 ];
 
-const swVersion = '1.206';
+const swVersion = '1.207';
 const cacheName = 'stsk-portal-v' + swVersion;
 const dataCacheName = 'stsk-portal-data-v' + swVersion;
 
