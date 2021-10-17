945e4bc8b6c21d9b522be2ed6c27e71b6ac39faf
stsk <stsk3@hotmail.com>
2021-10-17 22:00:44 +0800

    ADD optgroup to bus interchange select box FIX layout fo eta bus FIX bus-route.js

--- serviceworker.js
+++ serviceworker.js
@@ -32,7 +32,7 @@ const filesToCache = [
     "script/bus-route.js"
 ];
 
-const swVersion = '1.210';
+const swVersion = '1.211';
 const cacheName = 'stsk-portal-v' + swVersion;
 const dataCacheName = 'stsk-portal-data-v' + swVersion;
 
