59919c8c7a0316872f688390cd8e431f36c71d61
stsk <stsk3@hotmail.com>
2021-10-18 22:15:06 +0800

    FIX bus-eta wordings OPTIMIZE mtr wordings

--- serviceworker.js
+++ serviceworker.js
@@ -32,7 +32,7 @@ const filesToCache = [
     "script/bus-route.js"
 ];
 
-const swVersion = '1.211';
+const swVersion = '1.212';
 const cacheName = 'stsk-portal-v' + swVersion;
 const dataCacheName = 'stsk-portal-data-v' + swVersion;
 
