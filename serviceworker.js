66bacd8e479300c96ff51b767adffbbb608a9836
stsk <stsk3@hotmail.com>
2021-10-25 23:55:59 +0800

    FIX UX

--- serviceworker.js
+++ serviceworker.js
@@ -32,7 +32,7 @@ const filesToCache = [
     "script/bus-route.js"
 ];
 
-const swVersion = '1.216';
+const swVersion = '1.217';
 const cacheName = 'stsk-portal-v' + swVersion;
 const dataCacheName = 'stsk-portal-data-v' + swVersion;
 
