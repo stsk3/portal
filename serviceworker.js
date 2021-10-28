51f7f43f37cb1920cb7809d2d1a3abb73b5410b7
stsk3 <53324906+stsk3@users.noreply.github.com>
2021-10-28 19:08:44 +0800

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -36,7 +36,7 @@ const filesToCache = [
     "script/bus-route.js"
 ];
 
-const swVersion = '1.41';
+const swVersion = '1.42';
 const cacheName = 'stsk-portal-v' + swVersion;
 const dataCacheName = 'stsk-portal-data-v' + swVersion;
 
