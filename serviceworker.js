dcff80477e80b31b7b1471b31ea1b3f37c821cf2
stsk <stsk3@hotmail.com>
2021-11-15 20:54:37 +0800

    UPDATE bus eta to group bus stop platform

--- serviceworker.js
+++ serviceworker.js
@@ -36,7 +36,7 @@ const filesToCache = [
     "script/bus-route.js"
 ];
 
-const swVersion = '1.50';
+const swVersion = '1.51';
 const cacheName = 'stsk-portal-v' + swVersion;
 const dataCacheName = 'stsk-portal-data-v' + swVersion;
 
