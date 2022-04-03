5afe048a1492f0638c136117bf6e9b5fed180682
stsk3 <53324906+stsk3@users.noreply.github.com>
2022-04-04 03:04:14 +0800

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -46,7 +46,7 @@ const filesToCache = [
     "transport/sectional-tuen-yuen-tin.png",
 ];
 
-const swVersion = '1.81';
+const swVersion = '1.82';
 const cacheName = 'stsk-portal-v' + swVersion;
 const dataCacheName = 'stsk-portal-data-v' + swVersion;
 
