bb6f8fbb4c1fdcc29416b229206a143c79299562
stsk <stsk3@hotmail.com>
2022-04-25 12:51:54 +0800

    Add work eta for Whampoa

--- serviceworker.js
+++ serviceworker.js
@@ -67,7 +67,7 @@ const filesToCache = [
     "transport/sectional-tuen-yuen-tin.png",
 ];
 
-const swVersion = '1.97';
+const swVersion = '1.100';
 const cacheName = 'stsk-portal-v' + swVersion;
 const dataCacheName = 'stsk-portal-data-v' + swVersion;
 
