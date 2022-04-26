ffe39ed19a9f1f99cfe130c9fb5df3a6e64ad558
stsk <stsk3@hotmail.com>
2022-04-26 22:31:26 +0800

    Fix 72X

--- serviceworker.js
+++ serviceworker.js
@@ -67,7 +67,7 @@ const filesToCache = [
     "transport/sectional-tuen-yuen-tin.png",
 ];
 
-const swVersion = '1.101';
+const swVersion = '1.102';
 const cacheName = 'stsk-portal-v' + swVersion;
 const dataCacheName = 'stsk-portal-data-v' + swVersion;
 
