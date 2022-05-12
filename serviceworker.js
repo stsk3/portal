6c072babbcda638d84b78c05940e4c5681a358cf
stsk <stsk3@hotmail.com>
2022-05-12 23:35:55 +0800

    Add 307P

--- serviceworker.js
+++ serviceworker.js
@@ -67,7 +67,7 @@ const filesToCache = [
     "transport/sectional-tuen-yuen-tin.png",
 ];
 
-const swVersion = '1.104';
+const swVersion = '1.105';
 const cacheName = 'stsk-portal-v' + swVersion;
 const dataCacheName = 'stsk-portal-data-v' + swVersion;
 
