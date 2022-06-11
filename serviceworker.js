21b2019457f94b3928d25fb7c143173abea49a21
stsk <stsk3@hotmail.com>
2022-06-11 23:45:56 +0800

    Fix mtr bus eta when no record

--- serviceworker.js
+++ serviceworker.js
@@ -67,7 +67,7 @@ const filesToCache = [
     "transport/sectional-tuen-yuen-tin.png",
 ];
 
-const swVersion = '1.110';
+const swVersion = '1.111';
 const cacheName = 'stsk-portal-v' + swVersion;
 const dataCacheName = 'stsk-portal-data-v' + swVersion;
 
