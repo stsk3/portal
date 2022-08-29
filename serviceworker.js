ece5704f399729153973c1cbcfabcda16cc9cc51
skszeto <stsk3@hotmail.com>
2022-08-29 16:48:56 +0800

    FIX typo

--- serviceworker.js
+++ serviceworker.js
@@ -88,7 +88,7 @@ const filesToCache = [
     "transport/sectional-north.png",
 ];
 
-const swVersion = '1.159';
+const swVersion = '1.1591';
 const cacheName = 'stsk-portal-v' + swVersion;
 const dataCacheName = 'stsk-portal-data-v' + swVersion;
 
