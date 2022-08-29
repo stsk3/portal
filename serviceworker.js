1d6c6e02b51d949e430f3ac00de2c3d015431890
skszeto <stsk3@hotmail.com>
2022-08-29 17:02:19 +0800

    Update finance-bill table

--- serviceworker.js
+++ serviceworker.js
@@ -88,7 +88,7 @@ const filesToCache = [
     "transport/sectional-north.png",
 ];
 
-const swVersion = '1.1591';
+const swVersion = '1.1592';
 const cacheName = 'stsk-portal-v' + swVersion;
 const dataCacheName = 'stsk-portal-data-v' + swVersion;
 
