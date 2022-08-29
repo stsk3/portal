885b3bc4985fb961a0aa6a2f23f917cf8c903cd1
skszeto <stsk3@hotmail.com>
2022-08-29 16:08:37 +0800

    Fix home top nav bar layout

--- serviceworker.js
+++ serviceworker.js
@@ -88,7 +88,7 @@ const filesToCache = [
     "transport/sectional-north.png",
 ];
 
-const swVersion = '1.156';
+const swVersion = '1.157';
 const cacheName = 'stsk-portal-v' + swVersion;
 const dataCacheName = 'stsk-portal-data-v' + swVersion;
 
