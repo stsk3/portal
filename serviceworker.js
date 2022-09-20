0132f50ff1f613aca3270f3a652bc4269193d135
stsk <stsk3@hotmail.com>
2022-09-21 01:34:54 +0800

    Update birthday layout Add timetable for work eta

--- serviceworker.js
+++ serviceworker.js
@@ -88,7 +88,7 @@ const filesToCache = [
     "transport/sectional-north.png",
 ];
 
-const swVersion = '1.160';
+const swVersion = '1.161';
 const cacheName = 'stsk-portal-v' + swVersion;
 const dataCacheName = 'stsk-portal-data-v' + swVersion;
 
