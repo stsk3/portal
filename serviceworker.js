4822d0a66be32eee7efe3b060e36a05686569e63
skszeto <stsk3@hotmail.com>
2022-12-21 17:33:40 +0800

    Create import data function for finance bill

--- serviceworker.js
+++ serviceworker.js
@@ -101,7 +101,7 @@ const filesToCache = [
     nwPrefix + "NWFB/",
 ];
 
-const swVersion = '1.184';
+const swVersion = '1.185';
 const cacheName = 'stsk-portal-v' + swVersion;
 const dataCacheName = 'stsk-portal-data-v' + swVersion;
 
