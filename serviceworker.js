cab5ad028e9891452518b6156be028bdaecb8b86
Szeto <szeto@webglsoft.com>
2023-08-14 10:24:12 +0100

    Update TVSTSK app + Channel list Update exchange rate title and icon Update translation

--- serviceworker.js
+++ serviceworker.js
@@ -108,7 +108,7 @@ const filesToCache = [
     nwPrefix + "NWFB/",
 ];
 
-const swVersion = '1.43';
+const swVersion = '1.44';
 const cacheName = 'stsk-portal-v' + swVersion;
 const dataCacheName = 'stsk-portal-data-v' + swVersion;
 
