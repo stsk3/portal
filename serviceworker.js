a5b906a934bf0f216fe80293b195582e9baa57cc
stsk <stsk3@hotmail.com>
2023-10-02 19:35:40 +0100

    Fix finance API by adding token Fix finance bill import word limit

--- serviceworker.js
+++ serviceworker.js
@@ -110,7 +110,7 @@ const filesToCache = [
     nwPrefix + "NWFB/",
 ];
 
-const swVersion = '1.49';
+const swVersion = '1.50';
 const cacheName = 'stsk-portal-v' + swVersion;
 const dataCacheName = 'stsk-portal-data-v' + swVersion;
 
