842a0a33369bb123c2441568e021fbe32a3773c3
stsk3 <53324906+stsk3@users.noreply.github.com>
2024-09-25 00:09:40 +0100

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -110,7 +110,7 @@ const filesToCache = [
     nwPrefix + "NWFB/",
 ];
 
-const swVersion = '1.51';
+const swVersion = '1.52';
 const cacheName = 'stsk-portal-v' + swVersion;
 const dataCacheName = 'stsk-portal-data-v' + swVersion;
 
